0987544aa502592dd620bbf830ddcba010a4a05f
cyrus mwendwa <cyrusmwendwa370@gmail.com>
2024-01-02 18:22:57 +0300

    manage categories

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'dashboard'
 urlpatterns = [ 
     path('' , views.index , name='dashboard-index'),
+    path('manage-categories' , views.manage_categories , name='manage-categories'),
+    path('ajax-categories-fetch' , views.ajax_categories_fetch , name='ajax-categories-fetch'),
+    path('category/<int:category_id>' , views.category_detail , name='category-detail'),
+    path('edit-category/' , views.edit_category , name='edit-category'),
                
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,14 @@
 from hotel.models import *
 from accounts.models import *
 from django.utils import timezone
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect , JsonResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import render, get_object_or_404
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
+from django.core.serializers import serialize
+
 
 
 @login_required(login_url='accounts:login')
@@ -37,5 +39,67 @@ def index(request):
 
     return render(request, 'dashboard/home.html' ,  context)
 
+# manage  rooms view
+@login_required(login_url='accounts:login')
+@staff_member_required(login_url='accounts:login')
+def manage_categories(request):
+    categories = RoomCategory.objects.all()
+    context = {
+        'categories': categories,
+    }
+    return render(request, 'dashboard/hotel/manage_categories.html', context)
+
+def ajax_categories_fetch(request):
+    categories = RoomCategory.objects.all()
+    data = []
+
+    for category in categories:
+        data.append({
+            'id': category.id,
+            'name': category.name,
+            'image': category.image.url, 
+            'cost': category.cost,
+            'date': category.added_on.strftime('%b %d, %Y'),
+            'rooms': category.rooms,
+            'beds': category.beds,
+            'capacity': category.capacity,
+            
+        })
+
+    return JsonResponse({'data': data})
+
+def category_detail(request, category_id):
+    category = get_object_or_404(RoomCategory, pk=category_id)
+    context = {
+        'category': category,
+    }
+    return render(request, 'dashboard/hotel/edit_category.html', context)
 
 
+def edit_category(request ):
+   if request.method == 'POST':
+       category_id = request.POST['category_id']
+       try: 
+            category = RoomCategory.objects.get(id=category_id)
+            category.name = request.POST['name']
+            category.cost = request.POST['cost']
+            category.beds = request.POST['beds']
+            category.rooms = request.POST['rooms']
+            category.capacity = request.POST['capacity']
+            category.description = request.POST['description']
+            if 'new_image' in request.FILES:
+                 category.image = request.FILES['new_image']
+            
+            category.save()
+            success_message = 'Category updated successfully'
+            return HttpResponseRedirect(reverse('dashboard:category-detail', args=(category.id,)) + f'?success_message={success_message}')
+        
+
+       except Exception as e:
+            error_message = 'Error occurred while updating category'
+            return HttpResponseRedirect(reverse('dashboard:category-detail', args=(category.id,)) + f'?error_message={error_message}')
+          
+   else:
+        error_message = 'Invalid request'
+        return HttpResponseRedirect(reverse('dashboard:category-detail', args=(category.id,)) + f'?error_message={error_message}')
+            
